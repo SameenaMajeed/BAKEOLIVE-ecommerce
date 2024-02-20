2ca610a2665834aecd49bcf2475b54cd0d7ba93a
Sameena A Majeed <sameenamajeed611@gmail.com>
2024-02-20 19:32:01 +0530

    url changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sameenamajeed611:sameena@bakeolive.xb7xasz.mongodb.net/')
+mongoose.connect('mongodb+srv://sameenamajeed611:sameena@bakeolive.xb7xasz.mongodb.net/BAKEOlIVE')
 
 const {errorHandler,notFound}=require('./middleware/errorHandler');
 
